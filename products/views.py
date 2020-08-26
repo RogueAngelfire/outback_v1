694701101f61de50067edd32efd70455f9410305
Robin Collins <info@robincollins.co.uk>
2020-08-26 17:46:00 +0000

    added user title tags to serch

--- products/views.py
+++ products/views.py
@@ -20,14 +20,14 @@ def all_products(request):
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
-                products = products.annotate(lower_name=lower('name'))
-
-
-        if 'direction' in request.GET:
-            direction = request.GET['direction']
-            if direction == 'desc':
-                sortkey = f'-{sortkey}'
-        products = products.order_by(sortkey)
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
 
 
         if 'category' in request.GET:
@@ -44,7 +44,7 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-    current_sorting = f'(sort)_(direction)'
+    current_sorting = f'{sort}_(direction)'
 
     context = {
         'products': products,
