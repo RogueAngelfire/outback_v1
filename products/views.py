035d6c359744814057184f5f5b8d6cc36af9e190
Robin Collins <info@robincollins.co.uk>
2020-08-27 15:35:54 +0000

    Added add to bag functionality

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 # Create your views here.
@@ -29,8 +30,7 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -41,7 +41,7 @@ def all_products(request):
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
@@ -51,7 +51,7 @@ def all_products(request):
         'products': products,
         'search_term': query,
         'current_categories': categories,
-        "current_sorting": current_sorting,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
@@ -64,7 +64,6 @@ def product_detail(request, product_id):
 
     context = {
         'product': product,
-        'search_term': query,
     }
 
     return render(request, 'products/product_detail.html', context)
