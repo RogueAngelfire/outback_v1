91e37150c0f7da343e5e8c3bb0cc0a8c5686da15
Robin Collins <info@robincollins.co.uk>
2020-09-03 10:46:58 +0000

    Updated allath templates and tested profiles

--- checkout/views.py
+++ checkout/views.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
-from bag.contents import bag_contents
+from bag.contents import bag_contents 
 
 import stripe
 import json
